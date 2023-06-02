d3fbf5500c9eff30ecdb02a54bb7b76d607c858d
boypie510 <boypie510@gmail.com>
2023-06-02 18:49:51 +0800

    use seeds to create placements_teaser_data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# frozen_string_literal: true
+
+require 'json'
+
+# Read the JSON file
+file_path = Rails.root.join('db', 'placements_teaser_data.json')
+json_data = File.read(file_path)
+
+# Parse the JSON data
+data = JSON.parse(json_data)
+
+# Import data into the line_item and campaign tables
+
+ActiveRecord::Base.transaction do
+  data.each do |item|
+    campaign = Campaign.find_or_create_by(name: item['campaign_name'])
+    invoice = campaign.invoice || campaign.create_invoice!
+
+    campaign.line_items.create!(
+      {
+        invoice: invoice,
+        name: item['line_item_name'],
+        booked_amount: item['booked_amount'],
+        actual_amount: item['actual_amount'],
+        adjustments: item['adjustments']
+      }
+    )
+  end
+end
+
+puts 'Data imported successfully!'
